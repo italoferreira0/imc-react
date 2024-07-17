3a2bb3af6a3d7a4eed88b9aec18fa62d9b6d1cd5
Italo <italofsilva583@gmail.com>
2024-07-17 17:42:02 -0300

    atualização na DivMaster,trasparencia

--- src/components/DivMaster.jsx
+++ src/components/DivMaster.jsx
@@ -49,60 +49,57 @@ export default function DivMaster() {
     return(
         
         <div className='DivMaster'>
+    <div className="row">
+        <div className="col-md-6">
             <h2 className='title'>Calcular IMC</h2>
-            <div class="row ">  
-                
-                <div class="col w-25 d-inline-block DivPesoAltura ">
-                    <h3>Peso: </h3>
-                    <input type="number" className='Input ' placeholder='Quilos' value={peso} 
-                    onChange={event => {setPeso(event.target.value);}} step="0.01" min="0" max="10"/>
-                            
-                    <h3>Altura: </h3>
-                    <input type="number" className='Input' placeholder='Metros' value={altura} 
-                    onChange={event => {setAltura(event.target.value);}} step="0.01" min="0" max="10"/>
-
-                    <br /><br />
-                    <div>
-                    <h3>SEU IMC:</h3>
+            <div className="col w-100 DivPesoAltura">
+                <h3 className='title'>Peso:</h3>
+                <input type="number" className='Input' placeholder='Quilos' value={peso} 
+                       onChange={event => {setPeso(event.target.value);}} step="0.01" min="0" max="10"/>
+                <h3 className='title'>Altura:</h3>
+                <input type="number" className='Input' placeholder='Metros' value={altura} 
+                       onChange={event => {setAltura(event.target.value);}} step="0.01" min="0" max="10"/>
+                <br /><br />
+                <div>
+                    <h3 className='title'>SEU IMC:</h3>
                     <input type='number' className='Input' placeholder='Resultado' id='resultado' value={imc} readOnly/>
                     <br />
-                    <button type="button form-control" className='Button btn ' id='calcular' onClick={calcularImc}><strong>Calular</strong></button>            
-                        <button type="button form-control" className='Button btn ' id='limpar' onClick={limpar}><strong>Limpar</strong></button>
-                    </div>
-                    <br />
-                        
-                </div>       
-            </div>
-            <h3>Indices de </h3>	
-            <span><h3>IMC, Classificação:</h3></span>
-            <div class="w-20 d-inline-block DivTabela ">
-                        <table className=''>
-                        		
-                            <tr className='' id='magreza'  >
-                                <td><strong>Menor que 18,5: </strong></td>
-                                <td><strong>Magreza</strong></td>
-                            </tr>
-                            <tr id='normal' >
-                                <td><strong>Entre 18,5 e 24,9: </strong></td>
-                                <td><strong>Normal</strong></td>
-                            </tr>
-                            <tr id='sobrepeso' >
-                                <td><strong>Entre 25,0 e 29,9: </strong></td>
-                                <td><strong>Sobrepeso</strong></td>
-                            </tr>
-                            <tr id='obesidade' >
-                                <td><strong>Entre 30,0 e 39,9: </strong></td>
-                                <td><strong>Obesidade</strong></td>
-                            </tr>
-                            <tr id='grave' >
-                                <td><strong>Maior que 40,0: </strong></td>
-                                <td><strong>Obesidade Grave</strong></td>
-                            </tr>
-                    
-                    </table>
-
+                    <button type="button" className='Button btn' id='calcular' onClick={calcularImc}><strong>Calcular</strong></button>
+                    <button type="button" className='Button btn' id='limpar' onClick={limpar}><strong>Limpar</strong></button>
                 </div>
+                <br />
+            </div>
         </div>
+        <div className="col-md-6">
+            <div className="col w-100 DivTabela">
+                <h3>Índices de IMC, Classificação:</h3>
+                <table>
+                    <tr id='magreza'>
+                        <td><strong>Menor que 18,5:</strong></td>
+                        <td><strong>Magreza</strong></td>
+                    </tr>
+                    <tr id='normal'>
+                        <td><strong>Entre 18,5 e 24,9:</strong></td>
+                        <td><strong>Normal</strong></td>
+                    </tr>
+                    <tr id='sobrepeso'>
+                        <td><strong>Entre 25,0 e 29,9:</strong></td>
+                        <td><strong>Sobrepeso</strong></td>
+                    </tr>
+                    <tr id='obesidade'>
+                        <td><strong>Entre 30,0 e 39,9:</strong></td>
+                        <td><strong>Obesidade</strong></td>
+                    </tr>
+                    <tr id='grave'>
+                        <td><strong>Maior que 40,0:</strong></td>
+                        <td><strong>Obesidade Grave</strong></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+        
         
 
 
